e07fa9b60ca98c341f855facb4ec287679a99dc9
Rafael Kikuchi <rafaelk262002@gmail.com>
2025-12-22 14:08:23 -0300

    update test

--- src/app/relatorios/gdc/page.tsx
+++ src/app/relatorios/gdc/page.tsx
@@ -1,35 +1,32 @@
 "use client";
 
 import ProtectedLayout from "@/app/middleware/protectedLayout";
-import { useAuth } from "../../context/useUser";
-import { Navbar } from "@/components/all/navBar";
 import Perfil from "../../../../public/assets/perfil teste.avif";
 import Image from "next/image";
-import { useState } from "react";
+import { Navbar } from "@/components/all/navBar";
 
-export default function RelatorioGDC(){
+export default function Page() {
+  return (
+    <ProtectedLayout>
+      <main className="max-w-full h-screen flex">
+        <Navbar />
 
+        <main className="max-w-[84rem] w-full overflow-x-hidden xl:mx-auto">
+          <header className="w-full flex justify-end px-10 pt-6">
+            <Image
+              className="w-12 rounded-full border border-white"
+              src={Perfil}
+              alt="Perfil"
+            />
+          </header>
 
-    return(
-        <>
-            <ProtectedLayout>
-                <main className="max-w-full h-screen flex">
-                    <Navbar />
-                    <main className="max-w-[84rem] w-full overflow-x-hidden xl:mx-auto">
-                        <header className="w-full flex justify-end px-10 pt-6">
-                            <Image
-                            className="w-12 rounded-full border border-white"
-                            src={Perfil}
-                            alt="Perfil"
-                            />
-                        </header>
-
-                        <section className="w-full md:mt-14">
-                            <h1 className="font-bold text-4xl font-manrope">Relatório GDC</h1>
-                        </section>
-                    </main>
-                </main>
-            </ProtectedLayout>
-        </>
-    )
+          <section className="max-w-6xl w-full px-10 md:mt-14 md:mb-10">
+            <h1 className="font-bold text-4xl font-manrope">
+              Relatório de GDC
+            </h1>
+          </section>
+        </main>
+      </main>
+    </ProtectedLayout>
+  );
 }
